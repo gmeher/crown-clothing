40e8a2feaed22c1188c256655e3cb3ec82e9f392
Ghanashyam Meher <gmeher360@gmail.com>
2020-02-26 02:30:07 +0530

    optimized for production by applying conditional execution of  redux logger and composeWithDevTools while in development

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,19 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
 
-export const store = createStore(rootReducer, composeWithDevTools( applyMiddleware(...middlewares) ) );
+let state = 0;
+
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+    state = createStore(rootReducer, composeWithDevTools( applyMiddleware(...middlewares) ) );
+}else{
+    state = createStore(rootReducer,applyMiddleware(...middlewares));
+}
+
+export const store = state;
+
 
 export const persistor = persistStore(store);
 // export default {store, persistor};
